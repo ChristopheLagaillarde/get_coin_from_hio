9d249fe11104335bdb3e73a1b354b04c170d9c1d
ChristopheLagaillarde <chrislag94@gmail.com>
2022-05-22 22:20:07 +0400

    premier commit

---
+++ Credential_template.py
@@ -0,0 +1,11 @@
+class Credential:
+
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
