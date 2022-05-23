7a848515cdf0c5df052a4f13176e16eed78f8bd7
ChristopheLagaillarde <chrislag94@gmail.com>
2022-05-23 20:24:42 +0400

    ajout description et changement nom d'une methode

--- get_coin_from_hio.py
+++ get_coin_from_hio.py
@@ -1,13 +1,19 @@
-from selenium_tools.check_if_element_exist_with_id import check_if_element_exist_with_id
+# Programme : get_coin_from_hio
+# Description : This method go get the coin
+# Date : 23/05/2022
+# Author : Christophe Lagaillarde
+# Version : 1.0
+
+from selenium_tools.id_exist import id_exist
 from time import sleep
 from Credential import Credential
 
 
 def get_coin_from_hio(driver):
     driver.get("https://hogwarts.io/viewforum.php?f=52")
-    while check_if_element_exist_with_id("username", driver)\
-            and check_if_element_exist_with_id("password", driver)\
-            and check_if_element_exist_with_id("login", driver):
+    while id_exist("username", driver)\
+            and id_exist("password", driver)\
+            and id_exist("login", driver):
         sleep(1)
     my_login = Credential()
     driver.find_element_by_id("username").send_keys(my_login.get_username())
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Programme : GetMoneyFromHio
+# Description : This program excecute get_coin_from_hio
+# Date : 23/05/2022
+# Author : Christophe Lagaillarde
+# Version : 1.0
+
 from selenium_tools.while_making_automation_headless import while_making_automation_headless
 from get_coin_from_hio import get_coin_from_hio
 
