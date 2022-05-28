a8d3fc6a1e05dc67497ef54713f3ccb8d0a3cec2
ChristopheLagaillarde <chrislag94@gmail.com>
2022-05-28 08:51:05 +0400

    mise a jour

--- get_coin_from_hio.py
+++ get_coin_from_hio.py
@@ -7,15 +7,26 @@
 from selenium import webdriver
 from time import sleep
 from Credential import Credential
+from selenium_tools.id_exist import  id_exist
+from selenium_tools.name_exist import  name_exist
 
 
 def get_coin_from_hio(driver: webdriver) -> None:
     driver.get("https://hogwarts.io/viewforum.php?f=52")
-    sleep(20)
+
+    while id_exist('username', driver):
+        sleep(1)
     my_login = Credential()
     driver.find_element_by_id("username").send_keys(my_login.get_username())
+
+    while id_exist('password', driver):
+        sleep(1)
     driver.find_element_by_id('password').send_keys(my_login.get_password())
+
+    while name_exist('login', driver):
+        sleep(1)
     driver.find_element_by_name('login').click()
+
     del my_login
     sleep(20)
     driver.execute_script("document.getElementsByClassName(\"money money--sickles\")[1].click();")
